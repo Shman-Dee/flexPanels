68ce036b055313558d8a12de1bfd56068777aea6
Andrew McInally <realfunnyestate@yahoo.com>
2021-11-18 00:08:34 -0700

    adding js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const panels = document.querySelectorAll(".panels");
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+  this.classList.toggle("open");
+}
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", toggleOpen);
+  panel.addEventListener("transitionEndEvent", toggleActive);
+});
