7d376aba103a60bc626edbca10cc336e02c04a18
Andrew McInally <realfunnyestate@yahoo.com>
2021-11-22 18:43:52 -0700

    reworked and finished

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// const panels = document.querySelectorAll(".panel");
+const panels = document.querySelectorAll(".panel");
 
-// function toggleOpen() {
-//   this.classList.toggle("open");
-// }
+function toggleOpen() {
+  this.classList.toggle("open");
+  console.log("hello");
+}
 
-// function toggleActive(e) {
-//   if (e.propertyName.includes("flex")) {
-//     this.classList.toggle("open-active");
-//   }
-// }
-// panels.forEach((panel) => {
-//   panel.addEventListener("click", toggleOpen);
-//   panel.addEventListener("transitionEndEvent", toggleActive);
-// });
+function toggleActive(e) {
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
